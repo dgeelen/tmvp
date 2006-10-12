9231f22bfff7b3e17818d7e01298dae18d216583
simon.sasburg <simon.sasburg@b7710090-8d1f-0410-97c8-f1aef92249a0>
2006-10-12 12:33:18 +0000

    less mem, less speed(in theory)

--- compress1/lz77s.cpp
+++ compress1/lz77s.cpp
@@ -51,7 +51,7 @@ int main(int argc, char* argv[])
 	{
 		mlen = 0;
 		if (instr.check(ifpos+3)) {
-			uint32 lookupind = (instr[ifpos] << 16) | (instr[ifpos+1] << 8) | (instr[ifpos+2] << 0);
+			uint32 lookupind = (instr[ifpos] << 0) | (instr[ifpos+1] << 8);// | (instr[ifpos+2] << 16);
 			if (lookup[lookupind] != NULL) {
 				deque<uint32> *clook = lookup[lookupind];
 				while ((clook->size() > 0) && ((*clook)[0]+0x7FFF < ifpos))
@@ -59,7 +59,7 @@ int main(int argc, char* argv[])
 				for (uint i = 0; i < clook->size(); ++i)
 				{
 					uint32 lind = (*clook)[i];
-					uint32 clen = 3;
+					uint32 clen = 2;
 					while (instr.check(ifpos+clen) && (clen < 255) && (instr[ifpos+clen] == instr[lind+clen]))
 						clen++;
 					if (clen > 3 && clen > mlen) {
@@ -73,7 +73,7 @@ int main(int argc, char* argv[])
 		if (mlen==0)
 		{
 			if (instr.check(ifpos+2)) {
-				uint32 lookupind = (instr[ifpos] << 16) | (instr[ifpos+1] << 8) | (instr[ifpos+2] << 0);
+				uint32 lookupind = (instr[ifpos] << 0) | (instr[ifpos+1] << 8);// | (instr[ifpos+2] << 16);
 				if (lookup[lookupind] == NULL) lookup[lookupind] = new deque<uint32>;
 				lookup[lookupind]->push_back(ifpos);
 			}
@@ -94,7 +94,7 @@ int main(int argc, char* argv[])
 			outstr.write(mlen);
 			while (instr.check(ifpos) && (mlen > 0)) {
 				if (instr.check(ifpos+2)) {
-					uint32 lookupind = (instr[ifpos] << 16) | (instr[ifpos+1] << 8) | (instr[ifpos+2] << 0);
+					uint32 lookupind = (instr[ifpos] << 0) | (instr[ifpos+1] << 8);// | (instr[ifpos+2] << 16);
 					if (lookup[lookupind] == NULL) lookup[lookupind] = new deque<uint32>;
 					lookup[lookupind]->push_back(ifpos);
 				}
