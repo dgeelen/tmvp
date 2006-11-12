3db99cf84657f6b51d56e01a59c89be99ae7a97d
simon.sasburg <simon.sasburg@b7710090-8d1f-0410-97c8-f1aef92249a0>
2006-11-12 10:36:20 +0000

    [scripts/encoder] -remove workaround made obsolete by updating cygwin to latest version -add hqdnd3d to video filter chain to improve compression -prettify the script a little

--- scripts/encode.sh
+++ scripts/encode.sh
@@ -1,5 +1,4 @@
 
-
 MPLAYER="/cygdrive/c/stuff/mplayer/mplayer/mplayer.exe"
 MENCODER="/cygdrive/c/stuff/mplayer/mplayer/mencoder.exe"
 ILEAVE="../ileave/ileave.exe"
@@ -10,42 +9,42 @@ NORMALIZER="../normalizer/normalize.exe"
 INFILE="$1"
 OUTFILE="out.tmv"
 
+# just to be sure?...
 rm vidfifo  &> /dev/null
 rm audfifo1 &> /dev/null
 rm audfifo2 &> /dev/null
 
+# create our fifo pipes
 mkfifo vidfifo
 mkfifo audfifo1
 mkfifo audfifo2
 
-#echo "---extracting audio---"
-
 echo ">Starting audio decoder (mplayer)"
-"$MPLAYER" "$INFILE" -vc null -vo null -af volnorm,resample=8000,channels=1:2:0:0:1:0,format=u8 -ao pcm:fast:file=audfifo1:nowaveheader \
--quiet &> aud.log &
+"$MPLAYER" "$INFILE" -vc null -vo null -ao pcm:fast:file=audfifo1:nowaveheader	\
+  -af volnorm,resample=8000,channels=1:2:0:0:1:0,format=u8			\
+  -quiet &> aud.log &
 
 echo ">Starting video decoder (mencoder)"
-"$MENCODER" "$INFILE" -o vidfifo -of rawvideo -ovc raw -vf filmdint=io=23976:20000,scale=160:100,format=rgb24 -oac copy \
--quiet &> vid.log &
- 
+"$MENCODER" "$INFILE" -o vidfifo -of rawvideo -ovc raw -oac copy 		\
+  -vf filmdint=io=23976:20000,hqdn3d,scale=160:100,hqdn3d,scale,format=rgb24	\
+  -quiet &> vid.log &
+
 echo ">Starting audio normalizer"
-# "$NORMALIZER" audfifo1 audfifo2 & <-- doest work
-# cat audfifo1 | "$NORMALIZER" > audfifo2 & <-- doest work
-# line below does seems to work.... , cygwin weirdness? deadlock? :P
-"$NORMALIZER" audfifo1 > audfifo2 &
+"$NORMALIZER" audfifo1 audfifo2 &
 
 echo ">Making file header"
 cat fmagic.txt > "$OUTFILE"
 cat blocks.fon >> "$OUTFILE"
 
-echo ">Starting imagizer+interleaver+compressor"
+echo ">Starting imagizer + interleaver + compressor"
   "$IMAGIZER" vidfifo 		\
 | "$ILEAVE" - audfifo2 - 	\
 | "$COMPRESS" 			\
->> "$OUTFILE";
+>> "$OUTFILE"
 
 echo ">DONE"
 
+# delete our fifo pipes
 rm vidfifo
 rm audfifo1
 rm audfifo2
