665aef46a3a109d6890b078e1c4f74c0a0b4fdc8
simon.sasburg <simon.sasburg@b7710090-8d1f-0410-97c8-f1aef92249a0>
2006-11-12 20:00:36 +0000

    [normalizer] now accepts 3rd parameter, scale factor (0-256, default 128) [ileave] now accepts 4th parameter, audio sample size (default 400) [imagizer1] now accepts 3rd parameter, pallete threshold value (default 250)

--- trunk/ileave/main.cpp
+++ trunk/ileave/main.cpp
@@ -19,10 +19,13 @@ int main(int argc, char* argv[])
 	string if2name = "-";
 	string ofname = "-";
 
+	uint audsize = 400;
+
 	// TODO: maybe read other cmd line params???
 	if (argc > 1) if1name = argv[1];
 	if (argc > 2) if2name = argv[2];
 	if (argc > 3) ofname  = argv[3];
+	if (argc > 4) audsize = atoi(argv[4]);
 
 	if ( if2name == "-")
 		return 0;
@@ -37,15 +40,14 @@ int main(int argc, char* argv[])
 	do {
 		read1 = fread(buffer, 1, 8000 + 48, if1handle);
 		fwrite(buffer, 1, 8000 + 48, ofhandle);
-		read2 = fread(buffer, 1, 400, if2handle);
-		fwrite(buffer, 1, 400, ofhandle);
-	} while (read1 != 0 && read2 != 0);
+		read2 = fread(buffer, 1, audsize, if2handle);
+		fwrite(buffer, 1, audsize, ofhandle);
+	} while (!feof(if1handle) && !feof(if2handle));
 
-	fclose(if1handle);
-	fclose(if2handle);
-	if (ofname != "-") fclose(ofhandle);
+	if (if1name != "-") fclose(if1handle);
+	                    fclose(if2handle);
+	if (ofname  != "-") fclose(ofhandle);
 	return 0;
 }
 //---------------------------------------------------------------------------
 
- 
--- trunk/normalizer/normalize.cpp
+++ trunk/normalizer/normalize.cpp
@@ -15,21 +15,24 @@ int main(int argc, char* argv[])
 	string ifname = "-";
 	string ofname = "-";
 
+	uint factor = 256 / 2;
+
 	// TODO: maybe read other cmd line params???
 	if (argc > 1) ifname = argv[1];
 	if (argc > 2) ofname = argv[2];
+	if (argc > 3) factor = atoi(argv[3]);
 
 	FILE* ifhandle = (ifname == "-") ? stdin  : fopen(ifname.c_str(), "rb");
 	FILE* ofhandle = (ofname == "-") ? stdout : fopen(ofname.c_str(), "wb");
 
 	uint8 buf[32*1024];
 
-	uint32 read = -1;
+	uint32 read;
 	while (!feof(ifhandle))
 	{
 		read = fread(buf, 1, 32*1024, ifhandle);
-		for (int i = 0; i < read; ++i)
-			buf[i] >>= 1;
+		for (uint i = 0; i < read; ++i)
+			buf[i] = buf[i] * factor / 256;
 		fwrite(buf, 1, read, ofhandle);
 	}
 
