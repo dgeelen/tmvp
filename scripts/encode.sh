f1b32acd5fa07f4d77c5fb43667eeae8566cdd20
daniel.geelen <daniel.geelen@b7710090-8d1f-0410-97c8-f1aef92249a0>
2006-11-30 10:46:02 +0000

    [scripts] Enhanced the encode script

--- scripts/encode.sh
+++ scripts/encode.sh
@@ -1,50 +1,132 @@
 #!/bin/sh
-MPLAYER="`which mplayer`"
-MENCODER="`which mencoder`"
-ILEAVE="../ileave/ileave"
-IMAGIZER="../imagizer1/imagize"
-COMPRESS="../compress1/lz77s"
-NORMALIZER="../normalizer/normalize"
+if [ "${2}" == "" ] ; then
+  MPLAYER="`which mplayer`"
+  MENCODER="`which mencoder`"
+  ILEAVE=../ileave
+  IMAGIZER=../imagize
+  COMPRESS=../lz77s
+  NORMALIZER=../normalize
+  FMAGIC="fmagic.txt"
+  FONT="blocks.fon"
+else
+  if [ "${2}" == "dafox" ] ; then
+    MPLAYER="`which mplayer`"
+    MENCODER="`which mencoder`"
+    ILEAVE=~/Projects/tmvp/ileave/ileave
+    IMAGIZER=~/Projects/tmvp/imagizer1/imagize
+    COMPRESS=~/Projects/tmvp/compress1/lz77s
+    NORMALIZER=~/Projects/tmvp/normalizer/normalize
+    FMAGIC="filemagic.dat"
+    FONT="blocks.fon"
+  else
+    echo "Error: Unknown developer!"
+    exit
+  fi
+fi
 
-INFILE="$1"
-OUTFILE="out.tmv"
+if [ ! -f "${MPLAYER}" ] ; then
+   echo "Error: MPlayer not found!"
+  exit
+fi
+
+if [ ! -f "${MENCODER}" ] ; then
+   echo "Error: MEncoder not found!"
+  exit
+fi
+
+if [ ! -f "${ILEAVE}" ] ; then
+   echo "Error: Audio Interleaver not found!"
+  exit
+fi
+
+if [ ! -f "${IMAGIZER}" ] ; then
+   echo "Error: Imagizer not found!"
+  exit
+fi
+
+if [ ! -f "${COMPRESS}" ] ; then
+   echo "Error: Compressor not found!"
+  exit
+fi
+
+if [ ! -f "${NORMALIZER}" ] ; then
+   echo "Error: Normalize not found!"
+  exit
+fi
+
+if [ ! -f "${FMAGIC}" ] ; then
+   echo "Error: Cannot find \`${FMAGIC}'! (Required for FileMagic)"
+  exit #GOTO EXIT :p
+fi
+
+if [ ! -f "${FONT}" ] ; then
+  echo "Error: Cannot find \`${FONT}'! (Required for FontData)"
+  exit
+fi
+
+if [ "${1}" != "" ] ; then
+  if [ "${1}" == "-" ] ; then
+    INFILE="/dev/stdin"
+    OUTFILE=test.tmv
+  else
+    INFILE="$1"
+    OUTFILE="`echo ${1} | sed 's:\(.*\)\(\.[^\.]*\):\1.tmv:'`"
+    if ! echo "${OUTFILE}" | grep tmv ; then
+      OUTFILE="${OUTFILE}.tmv"
+    fi
+    echo Transcoding \`${1}\' to \`${OUTFILE}\'
+  fi
+else
+  echo Warning: No input filename given, defaulting to \`/dev/stdin\'
+  INFILE="/dev/stdin"
+  OUTFILE=test.tmv
+fi
+
+if [ ! -f "${INFILE}" ] ; then
+  echo "Error: cannot find \`${INFILE}'"
+  exit
+fi
 
 # just to be sure?...
-rm vidfifo  &> /dev/null
-rm audfifo1 &> /dev/null
-rm audfifo2 &> /dev/null
+rm /tmp/vidfifo  &> /dev/null
+rm /tmp/audfifo1 &> /dev/null
+rm /tmp/audfifo2 &> /dev/null
 
 # create our fifo pipes
-mkfifo vidfifo
-mkfifo audfifo1
-mkfifo audfifo2
+mkfifo /tmp/vidfifo
+mkfifo /tmp/audfifo1
+mkfifo /tmp/audfifo2
 
 echo ">Starting audio decoder (mplayer)"
-"$MPLAYER" "$INFILE" -vc null -vo null -ao pcm:fast:file=audfifo1:nowaveheader	\
-  -af volnorm,resample=8000,channels=1:2:0:0:1:0,format=u8			\
+#Volnorm=2:1 => uses several samples for better accuracy. However results in ~1s of soft sound at the start of the file
+"$MPLAYER" "$INFILE" -vc null -vo null -ao pcm:fast:file=/tmp/audfifo1:nowaveheader	\
+  -af volnorm=2:1,resample=8000,channels=1:2:0:0:1:0,format=u8			\
   -quiet &> aud.log &
 
 echo ">Starting video decoder (mencoder)"
-"$MENCODER" "$INFILE" -o vidfifo -of rawvideo -ovc raw -oac copy 		\
-  -vf filmdint=io=23976:20000,hqdn3d,scale=160:100,hqdn3d,scale,format=rgb24	\
+#"$MENCODER" "$INFILE" -o /tmp/vidfifo -of rawvideo -ovc raw -oac copy 		\
+#  -vf filmdint=io=23976:20000,hqdn3d,scale=160:100,hqdn3d,scale,format=rgb24	\
+#  -quiet &> vid.log &
+"$MENCODER" "$INFILE" -o /tmp/vidfifo -of rawvideo -ovc raw -oac copy     \
+  -vf-add filmdint=io=23976:20000,scale=320:-2,pp7=0:1,unsharp,2xsai,scale=-1:-2,hqdn3d,dsize=160:100,scale=160:100,format=rgb24 \
   -quiet &> vid.log &
 
 echo ">Starting audio normalizer"
-"$NORMALIZER" audfifo1 audfifo2 &
+"$NORMALIZER" /tmp/audfifo1 /tmp/audfifo2 &
 
 echo ">Making file header"
-cat fmagic.txt > "$OUTFILE"
-cat blocks.fon >> "$OUTFILE"
+cat ${FMAGIC} > "$OUTFILE"
+cat ${FONT} >> "$OUTFILE"
 
 echo ">Starting imagizer + interleaver + compressor"
-  "$IMAGIZER" vidfifo 		\
-| "$ILEAVE" - audfifo2 - 	\
+  "$IMAGIZER" /tmp/vidfifo 		\
+| "$ILEAVE" - /tmp/audfifo2 - 	\
 | "$COMPRESS" 			\
 >> "$OUTFILE"
 
 echo ">DONE"
 
 # delete our fifo pipes
-rm vidfifo
-rm audfifo1
-rm audfifo2
+rm /tmp/vidfifo
+rm /tmp/audfifo1
+rm /tmp/audfifo2
