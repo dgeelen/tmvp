f769fe59a2ac75e9e0cd06ef9209a82f885fbd49
simon.sasburg <simon.sasburg@b7710090-8d1f-0410-97c8-f1aef92249a0>
2006-12-12 12:25:20 +0000

    [normalizer] make use of ../common/UTypes.h

--- trunk/normalizer/normalize.cpp
+++ trunk/normalizer/normalize.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-#include "UTypes.h"
+#include "../common/UTypes.h"
 #include <string>
 #include <stdio.h>
 #include <assert.h>
