4f2aa6b9ad21c4fccab1d1d5142b4c4645daff3f
daniel.geelen <daniel.geelen@b7710090-8d1f-0410-97c8-f1aef92249a0>
2006-12-15 11:09:07 +0000

    [scripts/encode.sh] Removed superfluous output added subtitler

--- scripts/encode.sh
+++ scripts/encode.sh
@@ -8,20 +8,22 @@ COMPRESS=../lz77s
 NORMALIZER=../normalize
 FMAGIC="fmagic.txt"
 FONT="asc_ord_hi.fon"
-VIDFILTERS="scale=160:100,format=rgb24"
+VIDFILTERS="filmdint,scale=160:100,format=rgb24"
+SUBTITLER="cat"
 
 if [ "${2}" == "dafox" ] ; then
   MPLAYER="`which mplayer`"
   MENCODER="`which mencoder`"
-  ILEAVE=~/Projects/tmvp/ileave/ileave
-#  IMAGIZER=~/Projects/tmvp/imagizer1/imagize
-  IMAGIZER=~/Projects/tmvp/imagizer1/kdev/imagize/optimized/src/imagize
-  COMPRESS=~/Projects/tmvp/compress1/lz77s
+  ILEAVE="/home/dafox/Projects/tmvp/ileave/ileave"
+#  IMAGIZER=/home/dafox/Projects/tmvp/imagizer1/imagize
+  IMAGIZER="/home/dafox/Projects/tmvp/imagizer1/kdev/imagize/optimized/src/imagize"
+  COMPRESS="/home/dafox/Projects/tmvp/compress1/lz77s"
   PBCAT=~/Projects/tmvp/pbcat/pbcat
-  NORMALIZER=~/Projects/tmvp/normalizer/normalize
+  NORMALIZER="/home/dafox/Projects/tmvp/normalizer/normalize"
+  SUBTITLER="/home/dafox/Projects/tmvp/subtitler/kdev/subtitler/optimized/src/subtitler"
   FMAGIC="fmagic.txt"
 #  FONT="blocks.fon"
-  VIDFILTERS="scale=320:-2,pp7=0:1,unsharp,2xsai,scale=-1:-2,hqdn3d,dsize=160:100,scale=160:100,format=rgb24"
+  VIDFILTERS="filmdint,scale=320:-2,pp7=0:1,unsharp,2xsai,scale=-1:-2,hqdn3d,dsize=160:100,scale=160:100,format=rgb24"
 elif [ "${2}" == "simon" ] ; then
   MPLAYER="/cygdrive/c/stuff/mplayer/mplayer/mplayer.exe"
   MENCODER="/cygdrive/c/stuff/mplayer/mplayer/mencoder.exe"
@@ -83,7 +85,7 @@ if [ "${1}" != "" ] ; then
     if ! echo "${OUTFILE}" | grep tmv > /dev/null ; then
       OUTFILE="${OUTFILE}.tmv"
     fi
-    echo Transcoding \`${1}\' to \`${OUTFILE}\'
+#    echo Transcoding \`${1}\' to \`${OUTFILE}\'
   fi
 else
   echo Warning: No input filename given, defaulting to \`/dev/stdin\'
@@ -97,10 +99,10 @@ if [ ! -f "${INFILE}" ] ; then
 fi
 
 # just to be sure?...
-rm /tmp/catfifo  &> /dev/null
-rm /tmp/vidfifo  &> /dev/null
-rm /tmp/audfifo1 &> /dev/null
-rm /tmp/audfifo2 &> /dev/null
+rm -rf /tmp/vidfifo  &> /dev/null
+rm -rf /tmp/audfifo1 &> /dev/null
+rm -rf /tmp/audfifo2 &> /dev/null
+rm -rf /tmp/catfifo  &> /dev/null
 
 # create our fifo pipes
 mkfifo /tmp/catfifo
@@ -108,44 +110,43 @@ mkfifo /tmp/vidfifo
 mkfifo /tmp/audfifo1
 mkfifo /tmp/audfifo2
 
-echo -n "Calculating fps... "
-FPS="`${MPLAYER} \"${INFILE}\" -frames 0 2>&1 | grep -o '[^0-9][0-9\.]* fps' | sed 's:[^0-9]::g'`"
-VIDFILTERS="filmdint=io=${FPS}:20000,${VIDFILTERS}"
-echo "detected as \`${FPS}' fps"
-
-echo ">Starting progress bar"
+#echo ">Starting progress bar"
 "${PBCAT}" "${INFILE}" /tmp/catfifo &
 
-echo ">Starting audio decoder (mplayer)"
+#echo ">Starting audio decoder (mplayer)"
 #Volnorm=2:1 => uses several samples for better accuracy. However results in ~1s of soft sound at the start of the file
 "$MPLAYER" /tmp/catfifo -vc null -vo null -ao pcm:fast:file=/tmp/audfifo1:nowaveheader	\
   -af volnorm=2:1,resample=8000,channels=1:2:0:0:1:0,format=u8			\
   ${3} \
   -quiet &> aud.log &
 
-echo ">Starting video decoder (mencoder)"
+#echo ">Starting video decoder (mencoder)"
 #"$MENCODER" "$INFILE" -o /tmp/vidfifo -of rawvideo -ovc raw -oac copy 		\
 #  -vf filmdint=io=23976:20000,hqdn3d,scale=160:100,hqdn3d,scale,format=rgb24	\
 #  -quiet &> vid.log &
 "$MENCODER" "$INFILE" -o /tmp/vidfifo -of rawvideo -ovc raw -oac copy     \
+  -ofps 20.0 \
   -vf-add $VIDFILTERS \
   ${3} \
   -quiet &> vid.log &
 
-echo ">Starting audio normalizer"
+#echo ">Starting audio normalizer"
 "$NORMALIZER" /tmp/audfifo1 /tmp/audfifo2 &
 
-echo ">Making file header"
+#echo ">Making file header"
 cat ${FMAGIC} > "$OUTFILE"
 cat ${FONT} >> "$OUTFILE"
 
-echo ">Starting imagizer + interleaver + compressor"
+#SUBTITLER="cat"
+#echo "SUBTITLER=${SUBTITLER}"
+#echo ">Starting imagizer + subtitler + interleaver + compressor"
   "$IMAGIZER" /tmp/vidfifo - 750 148 ${FONT} \
+| "${SUBTITLER}" \
 | "$ILEAVE" - /tmp/audfifo2 - 	\
 | "$COMPRESS" 			\
 >> "$OUTFILE"
-
-echo ">DONE"
+# "$PROGRESSBAR" size
+#echo ">DONE"
 
 # delete our fifo pipes
 rm /tmp/catfifo
