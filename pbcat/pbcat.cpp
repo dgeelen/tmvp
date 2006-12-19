a30be2bac824c5a38d23a8257a75e0b4c50454e1
simon.sasburg <simon.sasburg@b7710090-8d1f-0410-97c8-f1aef92249a0>
2006-12-19 11:46:39 +0000

    [scripts] hacky progress bar

--- pbcat/pbcat.cpp
+++ pbcat/pbcat.cpp
@@ -61,7 +61,84 @@ int main(int argc, char* argv[])
 	if (argc > 3) fsize = atoi(argv[3]);
 
 	FILE* ifhandle = (ifname == "-") ? stdin  : fopen(ifname.c_str(), "rb");
-	FILE* ofhandle = (ofname == "-") ? stdout : fopen(ofname.c_str(), "wb");
+	FILE* ofhandle = (ofname == "-") ? stdout : fopen(ofname.c_str(), "w");
+
+	char ibuf;
+	uint32 read;
+	uint32 total;
+	int lsize = 0,nsize;
+	string in,out;
+	do {
+		if (fread(&ibuf, 1, 1,  ifhandle) != 1)
+			ibuf = 0;
+		if ((ibuf != '\r')
+		&&  (ibuf != '\n')
+		&&  (ibuf != 0)) {
+			in.push_back(ibuf);
+		} else {
+			int frame;
+			int proc;
+			float fps;
+			sscanf(in.c_str(), "Pos: %*fs %if ( %i%%) %ffps", &frame, &proc, &fps);
+
+			if (frame != 0) {
+				if (proc > 0)
+					total = frame * 100 / proc;
+				else
+					total = frame;
+
+				int eta;
+
+				if (fps != 0)
+					eta = (total-frame)/fps;
+
+				out.clear();
+
+				out.push_back('[');
+
+				int i = 0;
+				for (; i < 50 && 2*i < proc; ++i)
+					out.push_back('#');
+
+				out.push_back('|');
+
+				for (;i < 50; ++i)
+					out.push_back('.');
+
+				out.push_back(']');
+
+				char sbuf[11];
+				snprintf(sbuf, 10, "%i", proc);
+
+				out.push_back(' ');
+				out += sbuf;
+				out += "%";
+
+				nsize = out.size();
+				while (out.size() < lsize)
+					out.push_back(' ');
+
+
+				lsize = nsize;
+
+				fprintf(stderr, "\r%s\r", out.c_str());
+			} else {
+				fprintf(ofhandle, "%s\n", in.c_str());
+				fflush(ofhandle);
+			}
+
+			in.clear();
+		}
+	} while (ibuf != 0);
+// Pos:   0.1s      1f ( 0%)  0.00fps Trem:   0min   0mb  A-V:0.000 [0:0]
+//	getch();
+	fprintf(stderr, "\n");
+	return 0;
+
+
+
+
+	return 0;
 
 	if (fsize == -1) {
 		struct stat statbuf;
@@ -70,11 +147,10 @@ int main(int argc, char* argv[])
 	}
 
 	int32 done = 0;
-	int lsize = 0,nsize;
-	uint32 read;
+	//uint32 read;
 	uint8 buf[BUFSIZE];
 
-	string out;
+
 	//fprintf(stderr, "\n");
 
 	do {
