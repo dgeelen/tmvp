6c7ac4b6c0b3da1561e9d60651d68972c4fbf3a0
daniel.geelen <daniel.geelen@b7710090-8d1f-0410-97c8-f1aef92249a0>
2006-12-19 19:12:20 +0000

    [scripts/ssa2srt.sh] Simple script to convert a ssa file to srt format

---
+++ trunk/scripts/ssa2srt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Converts a given (100% compatible) SSA subtitle file to SRT format
+
+
+if [ "${1}" != "" ] ; then
+  if [ "${2}" != "" ] ; then
+    events=0
+    c=2
+    echo 1 > ${2}
+    cat "${1}" \
+    | grep "Dialogue" \
+    | sed "/^Dialogue/s:\([^,]*,\)\([^,]*\),\([^,]*\)\(,.*\):\20 --> \30\n\4\n:" \
+    | sed "s:^,[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,::" \
+    | sed "s:\\\[nN]:\n:" \
+    | while read s ; do
+            if [ "${s}" != "" ] ; then
+              echo $s
+            else
+              echo
+              echo ${c}
+              c=`expr ${c} + 1`
+              fi
+              done \
+    | sed '$d' \
+    >> "${2}"
+  else
+    echo Need an output file!
+  fi
+else
+  echo Need an input file!
+fi
